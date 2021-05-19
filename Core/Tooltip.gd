84a0b5bd37dbd05042b3295c48f48dca5c427d25
MetRiko <18maximus8050@gmail.com>
2021-05-19 15:39:14 +0200

    Update GUI

--- Core/Tooltip.gd
+++ Core/Tooltip.gd
@@ -3,22 +3,75 @@ extends Control
 onready var tabCont = get_node("Control/TabContainer")
 var buttonNum := int()
 
+const data = [
+	{
+		'groupName': "Group 1",
+		'elements': [
+			{
+				'name': "Tank" 
+			},
+			{
+				'name': "Turret" 
+			},
+			{
+				'name': "Drill" 
+			}
+		]
+	},
+	{
+		'groupName': "Group 2",
+		'elements': [
+			{
+				'name': "Tank" 
+			},
+			{
+				'name': "Turret" 
+			},
+			{
+				'name': "Drill" 
+			}
+		]
+	},
+	{
+		'groupName': "Group 3",
+		'elements': [
+			{
+				'name': "Tank" 
+			},
+			{
+				'name': "Turret" 
+			},
+			{
+				'name': "Drill" 
+			}
+		]
+	}
+]
 
 func _ready():
+	var groupId = 0
 	for tab in tabCont.get_children():
-		for button in get_node("GridContainer").get_children():
+		var buttonId = 0
+		for button in tab.get_node("GridContainer").get_children():
 			buttonNum += 1
-			button.connect("mouse_entered", self, "button_enter", [buttonNum])
-			button.connect("mouse_exited", self, "button_exit")
+			button.connect("mouse_entered", self, "button_enter", [groupId, buttonId])
+			button.connect("mouse_exited", self, "button_exit", [groupId, buttonId])
+			buttonId += 1
+		groupId += 1
 
-func button_enter(buttonNum):
-	match buttonNum:
-		1:
-	print("button1")
-		2:
-	print("button2")
-		3:
+func button_enter(groupId, buttonId):
+	print(groupId, buttonId)
+	if buttonId < data[groupId].elements.size():
+		print(data[groupId].elements[buttonId].name)
+	else:
+		print("unknown element")
 	
+func button_exit(groupId, buttonId):
+	print(groupId, buttonId)
+	if buttonId < data[groupId].elements.size():
+		print(data[groupId].elements[buttonId].name)
+	else:
+		print("unknown element")
 
 
 
