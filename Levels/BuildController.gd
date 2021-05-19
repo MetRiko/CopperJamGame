04d2cad385ccd678c1b5fa9ee543dc3988eb140b
Dualithe <dualithe@gmail.com>
2021-05-19 23:01:57 +0200

    Gizmos from sprite

--- Levels/BuildController.gd
+++ Levels/BuildController.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+onready var tilemap = Game.tilemap
 onready var level = Game.level
 onready var gui = Game.gui
 
@@ -8,10 +9,15 @@ var entityData
 func _ready():
 	gui.connect("module_button_pressed", self, "build_object")
 
-
 func build_object(moduleData):
 	entityData = moduleData.moduleId
-	
+	if entityData != null:
+		$Sprite.frame = moduleData.frameId
+		$Sprite.visible = true
+
+func _process(delta):
+	if entityData != null:
+		$Sprite.global_position = Vector2(tilemap.map_to_world(level.getCellIdxFromPos(get_global_mouse_position())))
 
 func _unhandled_input(event):
 	if event.is_action_pressed("LMB") && entityData != null:
@@ -19,3 +25,7 @@ func _unhandled_input(event):
 		entityData = null
 	elif event.is_action_pressed("RMB") && entityData != null:
 		entityData = null
+	if event.is_action_pressed("LMB") && entityData == null:
+		$Sprite.visible = false
+	elif event.is_action_pressed("RMB") && entityData == null:
+		$Sprite.visible = false
