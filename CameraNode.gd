d8d3a983b48ce2f67a1d2235a570325bd6d9b234
MetRiko <18maximus8050@gmail.com>
2021-05-23 15:37:17 +0200

    Change zoom factor + swap icons

--- CameraNode.gd
+++ CameraNode.gd
@@ -5,9 +5,9 @@ export var min_zoom := 0.5
 # Upper cap for the `_zoom_level`.
 export var max_zoom := 15
 # Controls how much we increase or decrease the `_zoom_level` on every turn of the scroll wheel.
-export var zoom_factor := 0.3
+export var zoom_factor := 0.2
 # Duration of the zoom's tween animation.
-export var zoom_duration := 0.15
+export var zoom_duration := 0.25
 
 # The camera's target zoom level.
 var _zoom_level := 1.0 setget _set_zoom_level
