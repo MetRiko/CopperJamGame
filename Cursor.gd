1a71b18e4aa4057139c5ccb143a1fac18371c7aa
VIOLIN MELODY <35158589+violinmelody@users.noreply.github.com>
2021-05-24 20:36:52 +0200

    Fix mouse pointer

--- Cursor.gd
+++ Cursor.gd
@@ -4,29 +4,20 @@ onready var level = Game.level
 
 onready var buildController = level.get_node("Controllers/BuildController")
 
+const CURSORS = {
+	0: 0,		# Boots
+	1: 1,		# Plus
+	2: 3,		# Wrench
+	3: 9		# Arrow
+}
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _process(delta):
 	position = get_global_mouse_position()
 	
-	if buildController.onEnemy == true and buildController.state != 2:
-		get_child(0).visible = false
-		get_child(1).visible = false
-		get_child(2).visible = false
-		get_child(3).visible = true
-	elif buildController.state == 0:
-		get_child(0).visible = true
-		get_child(1).visible = false
-		get_child(2).visible = false
-		get_child(3).visible = false
-	elif buildController.state == 1:
-		get_child(1).visible = true
-		get_child(0).visible = false
-		get_child(2).visible = false
-		get_child(3).visible = false
-	elif buildController.state == 2:
-		get_child(2).visible = true
-		get_child(0).visible = false
-		get_child(1).visible = false
-		get_child(3).visible = false
+	if buildController.onEnemy:
+		$Img.frame = 10
+	else:
+		$Img.frame = CURSORS[buildController.state]
